3fbd650e20a56d9b4730ea685e0f621817e73198
Hasib192 <hasibhosen7612@gmail.com>
2018-05-21 20:57:27 +0600

    initial

---
+++ 1005 - Average 1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main() {
+ 
+    double a, b,c,d;
+    scanf("%lf %lf", &a, &b);
+    c=3.5+7.5;
+    d=((a*3.5)+(b*7.5))/c;
+
+    printf("MEDIA = %.5lf\n", d);
+    
+    return 0;
+}
